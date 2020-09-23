623396020251d25d0f3d79b961f20a9fed5057c8
Chayce Knaub <chknaub@yahoo.com>
2020-09-23 14:31:42 -0400

    deckform working but have console warning about stric equals

--- src/DeckForm/DeckForm.js
+++ src/DeckForm/DeckForm.js
@@ -1,18 +1,54 @@
 import React from "react";
 import Navbar from "../NavBar/NavBar";
 import Context from "../Context";
-//THIS COMPONENT IS FOR BOTH /createdeck and /editdeck/:id ROUTES, that is why using match.params
-// if editing an existing deck, populate with existing data
+
 export default class DeckForm extends React.Component {
   static contextType = Context;
-  //if this.props.match.params === this.context.data.deck.id
+
   render() {
     const { id = 0 } = this.props.match.params;
-    console.log(this.props.match.params);
+    console.log(this.props.match.params.id);
+
+    //callback for filter method
+    // '===' doesn't work but getting console warning
+    const filterCards = (card) => {
+      return card.deckId == this.props.match.params.id;
+    };
+    const existingCardsInDeck = this.context.data.data.cards.filter(
+      filterCards
+    );
+    console.log(existingCardsInDeck);
+
+    const cardItem = existingCardsInDeck.map((card, i) => (
+      <li key={i} className="card-input-field">
+        <div className="card-q-a">
+          <label htmlFor="card-question"> Question</label>
+          <textarea
+            type="text"
+            className="card-question-input card-info"
+            name="card-question"
+            id="card-question"
+            defaultValue={card.question}
+          />
+          <label htmlFor="card-answer"> Answer</label>
+          <textarea
+            type="text"
+            className="card-answer-input card-info"
+            name="card-answer"
+            id="card-answer"
+            defaultValue={card.answer}
+          />
+          <button type="submit" className="delete-card">
+            {" "}
+            remove
+          </button>
+        </div>
+      </li>
+    ));
+
     return (
       <div className="edit-deck-page">
         <Navbar />
-
         <form>
           <h2>{id ? "Edit Deck" : "Create Deck"}</h2>
           <label htmlFor="deck-title">Deck Title</label>
@@ -22,36 +58,34 @@ export default class DeckForm extends React.Component {
             name="deck-title"
             id="deck-title"
           />
+          <h2>Cards</h2>
+          <ul className="card-inputs">
+            {cardItem}
+            {/* ask Tj about this key */}
+            <li key={cardItem[id]} className="card-input-field">
+              <div className="card-q-a">
+                <label htmlFor="card-question"> Question</label>
+                <textarea
+                  type="text"
+                  className="card-question-input card-info"
+                  name="card-question"
+                  id="card-question"
+                />
+                <label htmlFor="card-answer"> Answer</label>
+                <textarea
+                  type="text"
+                  className="card-answer-input card-info"
+                  name="card-answer"
+                  id="card-answer"
+                />
+                <button type="submit" className="delete-card">
+                  {" "}
+                  remove
+                </button>
+              </div>
+            </li>
+          </ul>
 
-          <div className="card-input-field">
-            <h2>Cards</h2>
-            {/* a div for existing cards? after saving a new card from form, becomes 
-            card front and back with edit button */}
-            <div className="card-q-a">
-              <label htmlFor="card-question"> Question</label>
-              <textarea
-                type="text"
-                className="card-question-input card-info"
-                name="card-question"
-                id="card-question"
-              />
-              <label htmlFor="card-answer"> Answer</label>
-              <textarea
-                type="text"
-                className="card-answer-input card-info"
-                name="card-answer"
-                id="card-answer"
-              />
-              <button type="submit" className="save-card">
-                {" "}
-                save
-              </button>
-            </div>
-          </div>
-          <button type="submit" className="add-new-card">
-            {" "}
-            New Card
-          </button>
           <button type="submit" className="save-deck-info">
             {" "}
             Save Changes
