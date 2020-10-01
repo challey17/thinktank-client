cc91603ac8794cb799205cda712c74c7d066d77d
Chayce Knaub <chknaub@yahoo.com>
2020-10-01 17:16:55 -0400

    add api endpoint to config

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  API_ENDPOINT: "http://localhost:8000/api",
-  //API_ENDPOINT: "",
+  //API_ENDPOINT: "http://localhost:8000/api",
+  API_ENDPOINT: "https://radiant-lowlands-24877.herokuapp.com/api",
   TOKEN_KEY: "thinktank-token",
 };
